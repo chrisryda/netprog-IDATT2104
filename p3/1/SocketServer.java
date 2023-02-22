f3c19f5be939e73d621d30b561bcf501941ec62b
chrisryda <chrisryda@hotmail.com>
2023-02-22 17:24:05 +0100

    Added single-threaded solution

---
+++ p3/1/SocketServer.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+
+class SocketServer {
+	public static void main(String[] args) throws IOException {
+		final int PORTNR = 1250;
+
+		ServerSocket server = new ServerSocket(PORTNR);
+		Socket connection = server.accept();
+		System.out.println("Server ready and waiting\n");
+
+		InputStreamReader readConnection = new InputStreamReader(connection.getInputStream());
+		BufferedReader reader = new BufferedReader(readConnection);
+		PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
+		writer.println("You are now connected to the server.");
+		writer.println("Write an expression adding or subtracting two numbers seperated by space,"
+				+ " e.g. '1 + 1', and I will send the result back to you.");
+
+		String expression = reader.readLine();
+		while (expression != null) {
+			System.out.println("A client wrote: " + expression);
+			writer.println("Expression recieved.");
+			double result = calculate(expression);
+			writer.println("Result of calculation: " + result);
+			expression = reader.readLine();
+		}
+
+		reader.close();
+		writer.close();
+		connection.close();
+		server.close();
+	}
+
+	public static double calculate(String expression) {
+		String[] data = expression.split(" ", 0);
+		if (data.length != 3) {
+			throw new IllegalArgumentException("Input is invalid.");
+		}
+
+		double a = Double.parseDouble(data[0]);
+		double b = Double.parseDouble(data[2]);
+		String operation = data[1];
+
+		double result;
+		switch (operation) {
+			case "+":
+				result = a + b;
+				break;
+			case "-":
+				result = a - b;
+				break;
+			case "/":
+				result = a / b;
+				break;
+			case "*":
+				result = a * b;
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid operator");
+		}
+		return result;
+	}
+}
